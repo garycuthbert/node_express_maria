0ed2ed30fb3e35a11364c9aa2086c057f337dfc3
Gary Cuthbert <gary.cuthbert@virgin.net>
2024-12-31 15:34:15 +0000

    latest progress, everything except encryption on password. Also getting BigInt parsing error trying to json parse response from mariabdb query

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const dotEnv = require('dotenv');
+
+dotEnv.config({
+    path: './.env-local'
+});
 
 const PORT = process.env.PORT | "3000";
 
 const app = express();
+
 
 /**
  * middleware
@@ -14,9 +20,12 @@ app.use(express.urlencoded({extended: false}));
  * routes
  */
 app.get('/', (request, response) => {
-    response.status(200).json({ name: "garyc", doing:"coding"});
+    response.status(200).send("This is not why you're here. Head to /user/:id and replace :id with your user id");
 });
 
+
+const userRouter = require('./routes/user');
+app.use('/user', userRouter);
 /**
  * start listening 
  */
